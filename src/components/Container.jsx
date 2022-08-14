732a4f7a10643b07df755c7f2d3967d3bcfe8ac1
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2022-08-14 20:58:13 +0500

    anim

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function Container({children}) {
+export default function Container({ children }) {
   return (
-    <section className='container'>
-          <h3 className="t">But hi (:</h3>
+    <section className="container">
+      {/* <h3 className="bored font-f f5">But hi (:</h3> */}
+      <h3 className="bored font-f2 tc fh ">
+        Bored
+        <span className="bored font-f2 in-bl  f6">af</span>
+      </h3>
+
       {children}
     </section>
-  )
+  );
 }
