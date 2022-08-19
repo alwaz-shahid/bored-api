cb11ac7abebecca085383eee06dbe1a338fc8b8f
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2022-08-19 19:59:01 +0500

    box animation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <nav className='h-[30px] bg-red-500'>
+    <nav className='h-[30px] bg-red-500 animate-box-grow'>
       <h1 className='animate-fd '>Nav</h1>
     </nav>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,67 +7,90 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        'fade-in-down': {
-            '0%': {
-                opacity: '0',
-                transform: 'translateY(-10px)'
-            },
-            '100%': {
-                opacity: '1',
-                transform: 'translateY(0)'
-            },
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
-        'fade-out-down': {
-            'from': {
-                opacity: '1',
-                transform: 'translateY(0px)'
-            },
-            'to': {
-                opacity: '0',
-                transform: 'translateY(10px)'
-            },
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
         },
-        'fade-in-up': {
-            '0%': {
-                opacity: '0',
-                transform: 'translateY(10px)'
-            },
-            '100%': {
-                opacity: '1',
-                transform: 'translateY(0)'
-            },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
-        'fade-out-up': {
-            'from': {
-                opacity: '1',
-                transform: 'translateY(0px)'
-            },
-            'to': {
-                opacity: '0',
-                transform: 'translateY(10px)'
-            },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
         },
-        'moving-line': {
-            from: {
-                width: '0px',
-                opacity: '0',
-            },
-            to: {
-                width: '30%',
-                opacity: '0.6',
-            },
+        "box-grow": {
+          "0%": {
+            height: "0",
+            width: "0",
+            opacity: "0",
+            position: "absolute",
+            display: "none",
+            top: "0",
+            left: "0",
+          },
+          "25%": {
+            display: "block",
+            opacity: "1",
+            height: "50px",
+            width: "50px",
+            left: "10%",
+            backgroundColor: "red",
+          },
+          "50%": {
+            backgroundColor: "blue",
+            left: "20%",
+            width: "200px",
+          },
+          "75%": {
+            backgroundColor: "green",
+            left: "0%",
+            width: "100px",
+          },
+          "100%": {
+            backgroundColor: "yellow",
+            left: "90%",
+            width: "100%",
+          },
         },
-    },
-    animation: {
-        'fade-in-down': 'fade-in-down 0.5s ease-out',
-        'fade-out-down': 'fade-out-down 0.5s ease-out',
-        'fade-in-up': 'fade-in-up 0.5s ease-out',
-        'fade-out-up': 'fade-out-up 0.5s ease-out',
-        'moving-line': 'moving-line .8s ease .5s forwards',
-        'fd': 'fade-out-up 2s ease 3s forwards'
-    }
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-down": "fade-out-down 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
+        fd: "fade-out-up 4s ease-in-out 2s",
+        "box-grow": "box-grow 2s ease-in-out 1s",
+      },
     },
   },
   plugins: [],
 };
-//  3s delay and forard
