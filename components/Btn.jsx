0743311a16233039a7be23bd70f8af8916379909
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2022-08-21 17:09:17 +0500

    btn

---
+++ components/Btn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Btn() {
+  return (
+    <div className="container">
+      <div className="box"></div>
+    </div>
+  )
+}
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Btn from "./Btn";
 import Emoji from "./Emoji";
 import Nav from "./Nav";
 
@@ -6,7 +7,7 @@ export default function Main({ children }) {
   return (
     <main className="min-h-screen overflow-hidden min-w-full max-w-7xl mx-auto bg-slate-800 scrollbar">
 
-      <Emoji />
+
       <Nav />
       {children}
     </main>
